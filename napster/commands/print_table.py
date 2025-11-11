f3228efb66a7176aa64fa436e8a8e195e1ba98cf
Guuzzeji <5lh8dl440f@gmail.com>
2025-11-10 22:43:19 -0800

    Clean up print table function, separate out functions

--- napster/commands/print_table.py
+++ napster/commands/print_table.py
@@ -1,14 +1,10 @@
+def __format_cell(text, column_width=15):
+    return str(text)[:column_width].ljust(column_width)
+
 def print_table(table, headers=None, column_width=15):
-    # Define a fixed column width
-
-    # Helper function for formatting a cell
-    def format_cell(text):
-        # Truncate and left-align the text within the column width
-        return str(text)[:column_width].ljust(column_width)
-
     if headers:
         # Format and join headers with a space
-        header_line = ' '.join([format_cell(header) for header in headers])
+        header_line = ' '.join([__format_cell(header, column_width) for header in headers])
         print(header_line)
 
         # Print separator line
@@ -17,5 +13,5 @@ def print_table(table, headers=None, column_width=15):
 
     for row in table:
         # Format and join row cells with a space
-        row_line = ' '.join([format_cell(cell) for cell in row])
+        row_line = ' '.join([__format_cell(cell, column_width) for cell in row])
         print(row_line)
