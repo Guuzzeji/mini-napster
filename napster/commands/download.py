e07853c9c3f61562aa877126217f2a4a2a67be3d
Guuzzeji <5lh8dl440f@gmail.com>
2025-11-22 23:16:17 -0800

    refactor download to only need file id

--- napster/commands/download.py
+++ napster/commands/download.py
@@ -1,6 +1,35 @@
 import time
 from napster.core.client import NapsterClient
 from napster.core.singleton import SingletonManager
+from napster.core.tracker_handler.tracker import get_download_info
+
+def download_command(command_input, download_clients, username):
+    if len(command_input) != 2:
+        print("Usage: download <file-id>")
+        print("Example: download abc-123")
+    else:
+        file_id = command_input[1]
+
+        user = get_download_info(file_id)
+        if user is None:
+            print(f"Error: Could not get download info for file ID '{file_id}' from tracker")
+            return
+
+        peer_key = (user.ip, user.port)
+
+        # Reuse existing client for this peer, or create a new one
+        if peer_key not in download_clients:
+            client = download(username, user.ip, user.port, file_id, user.file_name)
+            if client:
+                download_clients[peer_key] = client
+                print(f"Created new connection to peer {user.ip}:{user.port}")
+        else:
+            # Reuse existing client connection
+            client = download_clients[peer_key]
+            client.download_file(file_name="", file_id=file_id, checksum="")
+            print(f"Reusing existing connection to peer {user.ip}:{user.port}")
+            print(f"Download started for {user.file_name}")
+            print(f"Check download progress with 'dls' command")
 
 def download(username: str, target_ip: str, target_port: int, file_id: str, file_name: str):
     """
